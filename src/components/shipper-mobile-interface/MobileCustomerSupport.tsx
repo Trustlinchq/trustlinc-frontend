293d51a06614747cc348eac808ff24063f6acd49
Chiemela Nwankwo <twindrawingstudio@gmail.com>
2025-06-28 12:01:56 +0100

    Update MobileCustomerSupport.tsx

--- src/components/shipper-mobile-interface/MobileCustomerSupport.tsx
+++ src/components/shipper-mobile-interface/MobileCustomerSupport.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MessageSquareText } from "lucide-react";
+import { MessageSquareText, Play } from "lucide-react";
 import Link from "next/link";
 
 export default function MobileCustomerSupport() {
@@ -12,45 +12,52 @@ export default function MobileCustomerSupport() {
             </div>
 
             <div className="border-t border-border my-4" />
-            <Link href="/shipper/support">
-                <div className="flex items-center justify-between mb-3 pt-2">
-                    <div className="flex items-center gap-4">
-                        <div className="bg-neutral1 p-2 rounded-full">
-                            <MessageSquareText className="text-accent3 w-5 h-5" />
-                        </div>
-                        <div>
-                            <p className="font-semibold text-sm leading-tight text-backgroundSecondary/80">
-                                Talk to us
-                            </p>
-                            <p className="text-[10px] text-accent4/60 max-w-[85%]">
-                                Chat with someone from the TrustLinc community
-                                team,we’re happy to help.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="text-accent3 text-3xl">›</div>
-                </div>
-            </Link>
 
-            <Link href="https://www.youtube.com/@TrustLinc">
-                <div className="flex items-center justify-between mb-3 pt-2">
-                    <div className="flex items-center gap-4">
-                        <div className="bg-neutral1 p-2 rounded-full">
-                            <MessageSquareText className="text-accent3 w-5 h-5" />
+            {/* Support Links */}
+            <div className="flex flex-col gap-3">
+                <Link
+                    href="https://api.whatsapp.com/send/?phone=2348061463767&text=Hi%20TrustLinc%20Support%2C%20I%20need%20some%20help%20and%20I%E2%80%99m%20reaching%20out%20from%20your%20page.&type=phone_number&app_absent=0
+"
+                >
+                    <div className="flex items-center justify-between mb-3 pt-2">
+                        <div className="flex items-center gap-4">
+                            <div className="bg-neutral1 p-2 rounded-full">
+                                <MessageSquareText className="text-accent3 w-5 h-5" />
+                            </div>
+                            <div>
+                                <p className="font-semibold text-sm leading-tight text-backgroundSecondary/80">
+                                    Talk to us
+                                </p>
+                                <p className="text-xs text-accent4/60 max-w-[85%]">
+                                    Chat with someone from the TrustLinc
+                                    community team,we’re happy to help.
+                                </p>
+                            </div>
                         </div>
-                        <div>
-                            <p className="font-semibold text-sm leading-tight text-backgroundSecondary/80">
-                                Start Your Journey
-                            </p>
-                            <p className="text-[10px] text-accent4/60 max-w-[85%]">
-                                Watch short videos to see how TrustLinc makes
-                                sending and delivering easy.
-                            </p>
-                        </div>
+                        <div className="text-accent3 text-3xl">›</div>
                     </div>
-                    <div className="text-accent3 text-3xl">›</div>
-                </div>
-            </Link>
+                </Link>
+
+                <Link href="https://www.youtube.com/@TrustLinc">
+                    <div className="flex items-center justify-between mb-3 pt-2">
+                        <div className="flex items-center gap-4">
+                            <div className="bg-neutral1 p-2 rounded-full">
+                                <Play className="text-accent3 w-5 h-5" />
+                            </div>
+                            <div>
+                                <p className="font-semibold text-sm leading-tight text-backgroundSecondary/80">
+                                    Start Your Journey
+                                </p>
+                                <p className="text-xs text-accent4/60 max-w-[85%]">
+                                    Watch short videos to see how TrustLinc
+                                    makes sending and delivering easy.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="text-accent3 text-3xl">›</div>
+                    </div>
+                </Link>
+            </div>
         </div>
     );
 }
