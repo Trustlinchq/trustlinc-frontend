0068aa344bc19e671421395228d9f5218f76c7d0
Chiemela Nwankwo <twindrawingstudio@gmail.com>
2025-07-31 22:27:18 +0100

    Add custom headers for /og-image.jpg route
    Configured Next.js to set 'Cache-Control: no-store' and 'Access-Control-Allow-Origin: *' headers for the /og-image.jpg route to control caching and enable CORS.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    headers: async () => [
+        {
+            source: "/og-image.jpg",
+            headers: [
+                {
+                    key: "Cache-Control",
+                    value: "no-store",
+                },
+                {
+                    key: "Access-Control-Allow-Origin",
+                    value: "*",
+                },
+            ],
+        },
+    ],
 };
 
 export default nextConfig;
